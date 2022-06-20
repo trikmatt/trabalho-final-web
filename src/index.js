5c987674cf406d27b9fd4d2ee58e583bb35db151
Paulo <pvpmartins@hotmail.com>
2022-06-20 02:44:29 -0300

    refatorado

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header/header';
+
 import Footer from './components/Footer/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    
+      <App />
     <Footer />
   </React.StrictMode>
 );
