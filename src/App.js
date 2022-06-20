2384b9a2ea7900790c8a67e52b6ab19411e15794
Matt <mdovallesilva@gmail.com>
2022-06-20 09:41:41 -0300

    pull

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
      <h1>TRABALHO FINAL WEB</h1>
+     <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
     </div>
   );
 }
