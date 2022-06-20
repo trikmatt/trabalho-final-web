39c642042d432b4409d604e846471bbce5a5d530
Paulo <pvpmartins@hotmail.com>
2022-06-20 12:25:27 -0300

    identação aplicada

--- src/App.js
+++ src/App.js
@@ -53,14 +53,14 @@ function App() {
   return (
     <>
       <Header logarCliente={verificarCliente} clienteLogado={cliente} />
-    <div className="App">
-     <h1>Escolha algo abaixo para comprar</h1>
-     <div className="row row-cols-1 row-cols-md-2 g-4">
-     {produtos.map(produto=> 
-      <Card key={produto.idProduto} produto={produto} adicionar={adicionarAoCarrinho}></Card>
-      )}
-      </div>
-    </div>
+        <div className="App">
+          <h1>Escolha algo abaixo para comprar</h1>
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+            {produtos.map(produto=> 
+              <Card key={produto.idProduto} produto={produto} adicionar={adicionarAoCarrinho}></Card>
+            )}
+          </div>
+        </div>
       </>
   );
 }
