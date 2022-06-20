2b521f638e0846eb5be3ad108871dbf20e90bb7a
Paulo <pvpmartins@hotmail.com>
2022-06-20 02:37:05 -0300

    metodos de api implementados

--- src/App.js
+++ src/App.js
@@ -3,28 +3,65 @@ import React, { useEffect, useState } from 'react';
 import useAxiosGet from './hooks/useAxiosGet';
 import api from './services/api';
 import Card from './components/Card';
+import axios from 'axios';
+import useAxiosPost from './hooks/useAxiosPost';
+import Header from './components/Header/header';
 
 
 function App() {
   const [produtos, setProdutos] = useState([])
 
-  const {dados} = useAxiosGet('/produtos')
+  const {dados: getProdutos} = useAxiosGet('/produtos')
+
+  const {dados: getClientes} = useAxiosGet('/clientes')
+  
+  const {dados: postPedido} = useAxiosPost('/pedidos/item/1')
+
+  const [cliente, setCliente] = useState(null)
+
+  const [carrinho, setCarrinho] = useState([])
+
+  const verificarCliente = (nome) => {
+    const cliente = getClientes.filter(cliente => cliente.nome === nome)
+    if(cliente.length !== 0) {
+      setCliente(cliente[0])
+    }
+  }
+  useEffect(()=> {
+    console.log(cliente)
+  },[cliente])
+
+
+  const adicionarAoCarrinho = (id) => { 
+    const produto = produtos.filter(produto=>produto.idProduto === id)
+    setCarrinho([...carrinho, produto[0]])
+  }
 
   useEffect(()=>{
-    if(!dados) return
-    setProdutos(dados)
+    if(carrinho.length === 0) return
+    console.log(carrinho)
+  },[carrinho])
 
-  },[dados])
+
+
+  useEffect(()=>{
+    if(!getProdutos) return
+    setProdutos(getProdutos)
+
+  },[getProdutos])
 
   return (
+    <>
+      <Header logarCliente={verificarCliente} clienteLogado={cliente} />
     <div className="App">
      <h1>Escolha algo abaixo para comprar</h1>
      <div className="row row-cols-1 row-cols-md-2 g-4">
      {produtos.map(produto=> 
-      <Card key={produto.idProduto} produto={produto}></Card>
-     )}
+      <Card key={produto.idProduto} produto={produto} adicionar={adicionarAoCarrinho}></Card>
+      )}
       </div>
     </div>
+      </>
   );
 }
 
