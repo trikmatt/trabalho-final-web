65fe9b0d64045dc49fd790081b52ab179596e068
Paulo <pvpmartins@hotmail.com>
2022-06-21 13:09:59 -0300

    Lista de produtos do carrinho inserida com useLocation

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,10 +1,16 @@
 import Footer from "../../components/Footer/footer"
 import Header from "../../components/Header/header"
+import { useLocation } from "react-router"
 
 const Carrinho = () => {
+    const location = useLocation().state
+
     return (
         <>
         <Header/>
+            <ul>
+                {location && location.map((produto, index) => <li key={index}>{produto.nomeProduto}</li>)}
+            </ul>
         <Footer/>
         </>
     )
