9b943cc4886f989dd6c8f568f98865ddc5789958
Paulo <pvpmartins@hotmail.com>
2022-06-20 22:06:12 -0300

    Router component inserido

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer/footer';
-
+import Router from './Routes';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-      <App />
-    <Footer />
+      <Router/>
   </React.StrictMode>
 );
